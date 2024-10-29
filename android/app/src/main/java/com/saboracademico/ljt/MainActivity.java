e1c134d74eb1dfc71d97014192d075e9d67c167c
Trinidad Faccini <trinidad.faccini@theeye.io>
2024-10-29 14:18:55 -0300

    commit inicial

---
+++ android/app/src/main/java/com/saboracademico/ljt/MainActivity.java
@@ -0,0 +1,5 @@
+package com.saboracademico.ljt;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/environment/environment.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfo5AbDqF596pzer70mLdCX8QxJljUx4Y",
+    authDomain: "ppssegundoparcial-ljt.firebaseapp.com",
+    projectId: "ppssegundoparcial-ljt",
+    storageBucket: "ppssegundoparcial-ljt.appspot.com",
+    messagingSenderId: "111996389489",
+    appId: "1:111996389489:web:3ee23e175929a00aa3b3d0"
+  };
