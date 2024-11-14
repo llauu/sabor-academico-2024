f807698efd56e0375c70a9bd38560716f8a76b6a
rodriguezjulian <julian_rodriguez99@hotmail.com>
2024-11-14 20:15:47 -0300

    estructura componente confirmar pedidos

--- src/app/componentes/confirmar-pedidos/confirmar-pedidos.component.ts
+++ src/app/componentes/confirmar-pedidos/confirmar-pedidos.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FirestoreService } from '../../services/firestore.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-confirmar-pedidos',
   templateUrl: './confirmar-pedidos.component.html',
   styleUrls: ['./confirmar-pedidos.component.scss'],
+  imports : [CommonModule],
   standalone: true,
 })
-export class ConfirmarPedidosComponent  implements OnInit {
+export class ConfirmarPedidosComponent implements OnInit {
+  pedidos: any[] = [];
 
-  constructor() { }
+  constructor(private firestoreService: FirestoreService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.cargarPedidosPendientes();
+  }
 
+  cargarPedidosPendientes() {
+    this.firestoreService.getPedidos().subscribe((pedidosData) => {
+      this.pedidos = pedidosData
+        .filter((pedido: any) => pedido.estado === 'pendiente')
+        .map((pedido) => ({ ...pedido, mostrarDetalles: false }));
+    });
+  }
+
+  toggleDetalles(pedido: any) {
+    pedido.mostrarDetalles = !pedido.mostrarDetalles;
+  }
 }
