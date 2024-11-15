45ccea23360fc2e59dcff774bd8ac618d0abdb99
rodriguezjulian <julian_rodriguez99@hotmail.com>
2024-11-14 22:09:56 -0300

    update

--- src/app/componentes/confirmar-pago/confirmar-pago.component.ts
+++ src/app/componentes/confirmar-pago/confirmar-pago.component.ts
@@ -3,6 +3,7 @@ import { FirestoreService } from '../../services/firestore.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { IonButton } from '@ionic/angular/standalone';
+import {UserService} from '../../services/user.service'
 @Component({
   selector: 'app-confirmar-pago',
   templateUrl: './confirmar-pago.component.html',
@@ -13,7 +14,7 @@ import { IonButton } from '@ionic/angular/standalone';
 export class ConfirmarPagoComponent implements OnInit {
   pedidos: any[] = [];
 
-  constructor(private firestoreService: FirestoreService, private router: Router) {}
+  constructor(private firestoreService: FirestoreService, private router: Router,private userService: UserService) {}
 
   ngOnInit() {
     this.cargarPedidosPagados();
@@ -30,7 +31,29 @@ export class ConfirmarPagoComponent implements OnInit {
   }
 
   async confirmarPago(pedidoId: string) {
-    await this.firestoreService.deleteDocument(`listaPedidos/${pedidoId}`);
+    // Eliminar el pedido de la vista
     this.pedidos = this.pedidos.filter(pedido => pedido.id !== pedidoId);
+    console.log("pedido id " + pedidoId);
+  
+    try {
+      
+      const pedidoData: any = await this.firestoreService.getPedidoByUid(pedidoId);
+
+      const userId = pedidoData.userID;
+
+      const mesaSnapshot: any = await this.firestoreService.getMesaPorUserID(userId);
+      if (mesaSnapshot) {
+        const mesaUid = mesaSnapshot.id;
+        await this.firestoreService.updateDocument(`listaMesas/${mesaUid}`, {
+          'userID': ''
+        });
+      } else {
+        console.log('No se encontró ninguna mesa para el usuario.');
+      }
+    } catch (error) {
+      console.error('Error al confirmar el pago:', error);
+    }
+    await this.firestoreService.deleteDocument(`listaPedidos/${pedidoId}`);
   }
+  
 }
